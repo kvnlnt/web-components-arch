b9fea0422e4215bbe8a70a828fbe18bbb5ca9cf4
Kevin Lint <kevinlint@toolhouse.com>
2017-03-08 09:40:26 -0800

    add more parts to test

---
+++ parts/ark-text.js
@@ -0,0 +1,130 @@
+(function(PARTS) {
+
+    /**
+     * Class
+     *
+     * @class      ArkText (name)
+     * @param      {Object}  options  The options
+     */
+    function ArkText(options) {
+        var options = options || {};
+        this.el = options.el;
+        this.init();
+    };
+
+    // STATIC PROPERTIES
+
+    // Name of custom tag
+    ArkText.selector = "ark-text";
+
+    // params
+    ArkText.params = {
+        text: String,
+        image: String
+    };
+
+    // STATIC METHODS
+
+    /**
+     * css
+     *
+     * @param      {<type>}  data    The data
+     * @return     {Array}   array of css rules to be used by Ark.addCss
+     */
+    ArkText.css = function(data) {
+        return [
+            {
+                selector: ArkText.selector,
+                declarations: [
+                    'border: 5px solid ' + Ark.Page.theme.bgColor,
+                    'display: block',
+                    'padding: 20px',
+                    'max-width: 500px',
+                    'font-size: 1.2rem'
+                ]
+            },
+            {
+                selector: ArkText.selector + ' img',
+                declarations: [
+                    'float:right',
+                    'margin:2rem'
+                ]
+            }
+        ]
+    };
+
+    /**
+     * template
+     *
+     * @return     {string}  html template string which can be transformed by Ark.template
+     */
+    ArkText.template = function() {
+        var template = '';
+        template += '<% if(this.image) { %>';
+        template += '<img src="<% this.image %>"/>';
+        template += '<% } %>';
+        template += '<% for(item in this.text) { %>';
+        template += '<p><% this.text[item] %></p>';
+        template += '<% } %>';
+        return template;
+    };
+
+    /**
+     * initAll
+     * @desc  Creates an instance of ArkText for each ark-component tag found, also loads the css for the ArkText
+     */
+    ArkText.initAll = function() {
+        // find all ark-component tags
+        document.addEventListener(Ark.events.READY, function() {
+            Ark.Part.collectAndInitialize({
+                part: ArkText
+            });
+        });
+        // load the css
+        Ark.Css.add(ArkText.css());
+    };
+
+    // INSTANCE METHODS
+
+    ArkText.prototype = {
+        /**
+         * init
+         * @desc request component data and render on finish
+         *
+         * @return     {<type>}  { description_of_the_return_value }
+         */
+        init: function() {
+            var that = this;
+            return Ark.Api.getPart(this.getId(), function(err, data) {
+                that.render(ArkText.template(), data);
+            });
+        },
+        /**
+         * Gets the identifier.
+         *
+         * @return     {<type>}  The identifier.
+         */
+        getId: function() {
+            return this.el.getAttribute("id");
+        },
+        /**
+         * Renders this components template with data supplied
+         *
+         * @param      {Function}  template  The template
+         * @param      {<type>}    data      The data
+         * @return     {<type>}    the compiled template
+         */
+        render: function(template, data) {
+            var renderedTemplate = Ark.Template.compile(template, data);
+            this.el.innerHTML = renderedTemplate;
+            return renderedTemplate;
+        }
+    };
+
+    // look for any ark-components on this page and create instances of them
+    ArkText.initAll();
+
+    PARTS.ArkText = ArkText;
+    return PARTS;
+
+}(Ark.Parts || {}));
